3c09e6e347cfda91c27f8f9f917bd18338382764
nguyenluu <nguyetheluu1011317@gmail.com>
2021-08-21 07:43:11 +0700

    change scrtip tag

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,6 +1,6 @@
 import Document, { Html, Head, Main, NextScript, DocumentContext  } from 'next/document'
 import { ServerStyleSheet } from 'styled-components'
-
+import Script from 'next/script'
 export default class MyDocument extends Document {
   static async getInitialProps(ctx:DocumentContext) {
     const sheet = new ServerStyleSheet()
@@ -42,7 +42,7 @@ export default class MyDocument extends Document {
         <body>
           <Main />
           <NextScript />
-          <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossOrigin="anonymous"></script>
+          <Script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossOrigin="anonymous"></Script>          
         </body>
       </Html>
     )
