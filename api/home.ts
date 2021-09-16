9e39a23ab0bdcf4f80eff1fea953d31fbd0e260f
nguyenluu <nguyetheluu1011317@gmail.com>
2021-09-16 12:14:06 +0700

    Add etag

--- api/home.ts
+++ api/home.ts
@@ -1,21 +1,17 @@
 import axios from 'axios'
 
-export const getUserData = async (id = 1) => {
-    const devHeaders =
-        process.env.NODE_ENV === 'development'
-            ? {
-                  headers: {
-                      origin: 'http://localhost:3000',
-                  },
-              }
-            : undefined
+export const getUserData = async (id = 1) => {   
 
     const response = await axios.post(
         'https://nguyenluu.devcoin.ml/user',
         {
             userPhrase: 'nguyenluu-cv',
         },
-        devHeaders
+        {
+            headers: {
+                etag: 'add94b30605d0a4fbb86c1e6b03b2455',
+            },
+        }
     )
     return response.data
 }
