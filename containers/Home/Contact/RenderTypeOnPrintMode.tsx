2f86a06ee76829ea01bcc28b943cb7dd204cdd60
nguyenluu <nguyen.luu@finbase.vn>
2023-02-20 10:21:30 +0700

    chore(cms): fix cms

--- containers/Home/Contact/RenderTypeOnPrintMode.tsx
+++ containers/Home/Contact/RenderTypeOnPrintMode.tsx
@@ -2,6 +2,5 @@ import { decodePhoneNumber } from './utils'
 import type { RenderTypeProps } from './interface'
 
 export const RenderTypeOnPrintMode: React.FC<RenderTypeProps> = ({ type, linkProps, content }) => {
-    const renderContent = type === 'phone' ? decodePhoneNumber(content) : content
-    return <a {...linkProps}> {renderContent} </a>
+    return <a {...linkProps}> {content} </a>
 }
